abb6a70a1718a165a65853d3fb2570c5cdc6d270
Antti Kekki <antti.kekki@reaktor.com>
2025-05-04 20:23:34 +0300

    Fix R2 sync script

--- infra/post-glacial-rebound-data-r2/sync-map-data-to-r2.sh
+++ infra/post-glacial-rebound-data-r2/sync-map-data-to-r2.sh
@@ -1,5 +1,5 @@
 rclone sync \
-    ./map-data-processing/06_generate-map-distribution/result_cog \
+    ../../map-data-processing/06_generate-map-distribution/result_cog \
     r2:post-glacial-rebound-data/V1 \
     --include "*.tif" \
     --progress 
