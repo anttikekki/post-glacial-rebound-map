d266c09d7cfe14536529574bf81b5325713a76df
Antti Kekki <antti.kekki@reaktor.com>
2025-05-09 05:46:30 +0300

    Split Webpack output to chunks and add hash to bundle name

--- ui/webpack.config.mjs
+++ ui/webpack.config.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 
 export default (env, argv) => {
@@ -13,6 +14,10 @@ export default (env, argv) => {
     plugins: [
       new webpack.DefinePlugin({
         "process.env.MAANNOUSU_API": JSON.stringify(process.env.MAANNOUSU_API),
+      }),
+      new HtmlWebpackPlugin({
+        template: "src/index.ejs",
+        filename: "index.html",
       }),
     ],
     devtool: "source-map",
@@ -37,9 +42,19 @@ export default (env, argv) => {
     resolve: {
       extensions: [".ts", ".js"],
     },
+    performance: {
+      maxAssetSize: 1000000, // 1000 KiB (default is 250 KiB)
+      maxEntrypointSize: 1000000, // 1000 KiB (default is 250 KiB)
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+        minSize: 100000, // Minimum size for splitting (100 KiB)
+      },
+    },
     output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: "[name]-[contenthash].js",
+      path: path.resolve(__dirname, "public"),
       clean: true,
     },
   };
